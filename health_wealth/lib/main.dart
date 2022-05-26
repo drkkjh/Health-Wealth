0fb889bf57bbcae53bb755b60bf7df0983d869ac
Khoo Jing Hong, Derrick <derrickkhoo2000@gmail.com>
2022-05-26 18:43:46 +0800

    SnackTracker user interface

--- health_wealth/lib/main.dart
+++ health_wealth/lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:health_wealth/screens/home.dart';
+import 'package:health_wealth/screens/snacktracker.dart';
 
 void main() {
-  
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    debugShowCheckedModeBanner:
+    false;
+    return SnackTracker();
+  }
 }
