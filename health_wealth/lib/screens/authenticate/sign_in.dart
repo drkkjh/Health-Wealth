32466bf7f135cb424555c1b56f6a107c1b17e1ff
ish1506 <ishiuanlee@gmail.com>
2022-05-27 18:37:05 +0800

    Add loading widget for SignIn and Register and create decoration for TextFormField for SignIn and Register

--- health_wealth/lib/screens/authenticate/sign_in.dart
+++ health_wealth/lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:health_wealth/common/form_input_decoration.dart';
+import 'package:health_wealth/common/loading.dart';
 import 'package:health_wealth/services/auth.dart';
 
 /// The Sign In screen widget.
@@ -17,6 +19,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // User input variables
   String email = '';
@@ -26,90 +29,101 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.lightBlueAccent,
-      appBar: AppBar(
-        backgroundColor: Colors.indigoAccent,
-        elevation: 0.0,
-        title: const Text('Sign in'),
-        actions: [
-          TextButton.icon(
-            icon: const Icon(Icons.person),
-            label: const Text('Create account'),
-            onPressed: () {
-              widget.togglePage();
-            },
-          ),
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 20.0),
-              TextFormField(
-                validator: (input) {
-                  if (input != null && input.isEmpty) {
-                    return 'Enter your email';
-                  } else {
-                    return null;
-                  }
-                },
-                onChanged: (input) {
-                  setState(() => email = input);
-                },
-              ),
-              const SizedBox(height: 20.0),
-              TextFormField(
-                obscureText: true,
-                validator: (input) {
-                  if (input != null) {
-                    if (input.isEmpty) {
-                      return 'Enter your password';
-                    } else if (input.length < 6) {
-                      return 'Your password must be at least 6 characters!';
+    if (loading) {
+      return const Loading();
+    } else {
+      return Scaffold(
+        // backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.indigoAccent,
+          elevation: 0.0,
+          title: const Text('Sign in'),
+          actions: [
+            TextButton.icon(
+              icon: const Icon(Icons.person),
+              label: const Text('Create account'),
+              onPressed: () {
+                widget.togglePage();
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: formInputDecoration.copyWith(hintText: 'Email'),
+                  validator: (input) {
+                    if (input != null && input.isEmpty) {
+                      return 'Enter your email';
+                    } else {
+                      return null;
                     }
-                  }
-                  return null;
-                },
-                onChanged: (input) {
-                  setState(() => password = input);
-                },
-              ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+                  },
+                  onChanged: (input) {
+                    setState(() => email = input);
+                  },
                 ),
-                onPressed: () async {
-                  // Check if form is valid.
-                  if (_formKey.currentState != null) {
-                    if (_formKey.currentState!.validate()) {
-                      dynamic result = await _auth.signIn(email, password);
-                      if (result == null) {
-                        setState(() => errorMsg = 'Error with signing in');
+                const SizedBox(height: 20.0),
+                TextFormField(
+                  obscureText: true,
+                  decoration:
+                      formInputDecoration.copyWith(hintText: 'Password'),
+                  validator: (input) {
+                    if (input != null) {
+                      if (input.isEmpty) {
+                        return 'Enter your password';
+                      } else if (input.length < 6) {
+                        return 'Your password must be at least 6 characters!';
                       }
                     }
-                  }
-                },
-              ),
-              const SizedBox(height: 20.0),
-              Text(
-                errorMsg,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 20.0,
+                    return null;
+                  },
+                  onChanged: (input) {
+                    setState(() => password = input);
+                  },
                 ),
-              ),
-            ],
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () async {
+                    // Check if form is valid.
+                    if (_formKey.currentState != null) {
+                      if (_formKey.currentState!.validate()) {
+                        setState(() => loading = true);
+                        dynamic result = await _auth.signIn(email, password);
+                        if (result == null) {
+                          setState(() {
+                            loading = false;
+                            errorMsg = 'Error with signing in';
+                          });
+                        }
+                      }
+                    }
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                Text(
+                  errorMsg,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
