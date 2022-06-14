e6db6a51aa7c201c6ba7dfb4b87f9f8408b78159
Khoo Jing Hong, Derrick <derrickkhoo2000@gmail.com>
2022-06-14 17:13:47 +0800

    Updated RunTracker with live location tracking and necessary UIs

--- health_wealth/lib/screens/runtracker.dart
+++ health_wealth/lib/screens/runtracker.dart
@@ -1,6 +1,14 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:health_wealth/src/locations.dart' as locations;
+import 'package:health_wealth/model/runlist.dart';
+import 'package:health_wealth/screens/running.dart';
+import 'package:health_wealth/model/runningcard.dart';
+import 'package:provider/provider.dart';
+import '../model/runningdetails.dart';
+import '../services/auth.dart';
+import '../services/database.dart';
 
 class RunTracker extends StatefulWidget {
   const RunTracker({Key? key}) : super(key: key);
@@ -10,28 +18,65 @@ class RunTracker extends StatefulWidget {
 }
 
 class _RunTrackerState extends State<RunTracker> {
-  late GoogleMapController mapController;
+  late List<RunningDetails> _data;
+  late List<RunningCard> _cards;
+  final _auth = AuthService();
+  late final _db = DatabaseService(uid: _auth.currentUser.uid);
+  final user = AuthService().currentUser;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  void initState() {
+    super.initState();
+    _fetchDBEntries();
+  }
 
-  void _onMapCreated(GoogleMapController mapController) {
-    this.mapController = mapController;
+  void _fetchDBEntries() async {
+    _cards = []; // clear database
+    //_data = _results.map((item) => RunningDetails.fromMap(item)).toList();
+    //_data.forEach((value) => _cards.add(RunningCard(runningdetails: value)));
+    setState(() {
+      Stream<List<RunningDetails>> _results = _db.getRuns;
+    });
+  }
+
+  void _addDBEntry(RunningDetails rd) async {
+    _db.insertRun(rd);
+    _fetchDBEntries();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('RunTracker'),
-          centerTitle: true,
-          backgroundColor: Colors.lightBlue,
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    return Container(
+      height: height,
+      width: width,
+      child: StreamProvider<List<RunningDetails>>.value(
+        initialData: const [],
+        value: _db.getRuns,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("RunTracker"),
+            centerTitle: true,
+            backgroundColor: Colors.blue,
+          ),
+          body: const RunList(),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+            onPressed: () {
+              final activityToTrack = Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Running(),
+                ),
+              ).then((value) => _addDBEntry(value));
+              setState() {}
+            },
+            tooltip: 'Press to start tracking a run',
+            child: Icon(
+              Icons.add,
+            ),
           ),
         ),
       ),
--- health_wealth/lib/services/database.dart
+++ health_wealth/lib/services/database.dart
@@ -1,12 +1,23 @@
+import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:health_wealth/model/runningcard.dart';
+import 'package:health_wealth/model/runningdetails.dart';
+import 'package:health_wealth/screens/running.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as p;
 
 class DatabaseService {
   final String uid;
+  static late Database _db;
   DatabaseService({required this.uid});
 
   // Collection reference
   final CollectionReference usersCollection =
       FirebaseFirestore.instance.collection("users");
+
+  // Collection reference for runs
+  final CollectionReference runsCollection =
+      FirebaseFirestore.instance.collection("runs");
 
   Future createUser(String email) async {
     return await usersCollection
@@ -18,6 +29,32 @@ class DatabaseService {
     // TODO: Implement logic to check that username isn't taken
     return await usersCollection.doc(uid).update({'username': username});
     // .set({'username': username}, SetOptions(merge: true));
+  }
+
+  // For RunTracker specifically
+  Stream<List<RunningDetails>> get getRuns {
+    return runsCollection.snapshots().map(_snapshotToListOfRuns);
+  }
+
+  List<RunningDetails> _snapshotToListOfRuns(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      var data = doc.data() as Map<String, dynamic>;
+      return RunningDetails(
+        date: data['date'] ?? '',
+        duration: data['duration'] ?? '',
+        speed: data['speed'] ?? '',
+        distance: data['distance'] ?? '',
+      );
+    }).toList();
+  }
+
+  Future insertRun(RunningDetails details) async {
+    return await runsCollection.doc(uid).set({
+      'date': details.date,
+      'duration': details.duration,
+      'speed': details.speed,
+      'distance': details.duration,
+    });
   }
 }
 
